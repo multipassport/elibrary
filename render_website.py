91cdc09f077346c2aad1ae35f79607d9eeed7cc2
multipassport <mashross@gmail.com>
2021-05-28 12:09:07 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,7 +1,9 @@
 import json
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import chunked
 
 def get_template():
     env = Environment(
@@ -12,19 +14,26 @@ def get_template():
     return template
 
 
-def render_page():
+def render_pages():
     json_file = 'books_data.json'
+    book_chunk_size = 8
+    folder = 'pages'
+    os.makedirs(folder, exist_ok=True)
     with open(json_file, 'r', encoding='utf-8') as file:
         books_details = json.load(file)
     
-    rendered_page = get_template().render(
-        books=books_details,
-    )
-    with open('index.html', 'w', encoding='utf-8') as file:
-        file.write(rendered_page)
+    books_details_pages = list(chunked(books_details, book_chunk_size))
+    for page_number, page in enumerate(books_details_pages, 1):
+        rendered_page = get_template().render(
+            books=page,
+        )
+        page_path = os.path.join(folder, f'index{page_number}.html')
+        with open(page_path, 'w', encoding='utf-8') as file:
+            file.write(rendered_page)
+
 
 if __name__ == '__main__':
-    render_page()
+    render_pages()
 
     server=Server()
 
